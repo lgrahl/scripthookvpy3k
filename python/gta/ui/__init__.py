93f0a17b28fb52bb544b13a7b3746f3ee97dfe03
Lennart Grahl <lennart.grahl@gmail.com>
2015-06-21 15:16:29 +0200

    Add Distance class for the margin of an UI element Add shorthands for Point, Distance and Color classes Add builder method for Color so the static shorthands are not affected when changing the alpha value Remove padding property from Item Use shorthands in Item default property values

--- python/gta/ui/__init__.py
+++ python/gta/ui/__init__.py
@@ -19,6 +19,51 @@ class Point(collections.namedtuple('Point', 'x, y')):
     """
     pass
 
+# Point shorthands
+Point.TopLeft = Point(0.0, 0.0)
+Point.TopRight = Point(1.0, 0.0)
+Point.BottomLeft = Point(0.0, 1.0)
+Point.BottomRight = Point(1.0, 1.0)
+Point.Center = Point(0.5, 0.5)
+
+
+class Distance(collections.namedtuple('Distance', 'top, right, bottom, left')):
+    """
+    Represents the space around an UI element.
+
+    Arguments:
+        - `top`: Space around the top.
+        - `right`: Space around the right.
+        - `bottom`: Space around the bottom.
+        - `left:`: Space around the left.
+
+    Alternatively, a single CSS-like margin string can be supplied
+    with support for one to four values.
+    """
+    def __new__(cls, *args, **kwargs):
+        # Convert CSS-like margin string
+        distance, *_ = args
+        if isinstance(distance, str):
+            values = [float(value) for value in distance.strip().split(' ')]
+            top, right, bottom, left = [0.0] * 4
+            length = len(values)
+            if length == 1:
+                top = right = bottom = left = values[0]
+            elif length == 2:
+                top, right = values
+                bottom = top
+                left = right
+            elif length == 3:
+                top, right, bottom = values
+                left = right
+            elif length == 4:
+                top, right, bottom, left = values
+            return super(Distance, cls).__new__(cls, top, right, bottom, left)
+        return super(Distance, cls).__new__(cls, *args, **kwargs)
+
+# Distance shorthands
+Distance.Zero = Distance('0')
+
 
 class Color(collections.namedtuple('Color', 'r, g, b, a')):
     """
@@ -37,11 +82,171 @@ class Color(collections.namedtuple('Color', 'r, g, b, a')):
     """
     def __new__(cls, *args, a=1.0, **kwargs):
         # Convert CSS-like hexadecimal colour to integer values
-        color, *_ = args
-        if isinstance(color, str) and len(color) == 7 and color[0] == '#':
-            r, g, b = (color[i + 1:i + 3] for i in range(0, 6, 2))
+        if len(args) > 0 and isinstance(args[0], str):
+            color = args[0]
+            r, g, b = (int(color[i + 1:i + 3], base=16) for i in range(0, 6, 2))
             return super(Color, cls).__new__(cls, r, g, b, a)
         return super(Color, cls).__new__(cls, *args, a=a, **kwargs)
+
+    def alpha(self, value):
+        """
+        Return a new instance of the current colour with a different
+        alpha value.
+
+        Arguments:
+            - `value`: An alpha channel value between ``0`` and ``1``.
+        """
+        return Color(self.r, self.g, self.b, a=value)
+
+# Colour shorthands
+Color.Aqua = Color(0, 255, 255)
+Color.AliceBlue = Color(240, 248, 255)
+Color.AntiqueWhite = Color(250, 235, 215)
+Color.Black = Color(0, 0, 0)
+Color.Blue = Color(0, 0, 255)
+Color.Cyan = Color(0, 255, 255)
+Color.DarkBlue = Color(0, 0, 139)
+Color.DarkCyan = Color(0, 139, 139)
+Color.DarkGreen = Color(0, 100, 0)
+Color.DarkTurquoise = Color(0, 206, 209)
+Color.DeepSkyBlue = Color(0, 191, 255)
+Color.Green = Color(0, 128, 0)
+Color.Lime = Color(0, 255, 0)
+Color.MediumBlue = Color(0, 0, 205)
+Color.MediumSpringGreen = Color(0, 250, 154)
+Color.Navy = Color(0, 0, 128)
+Color.SpringGreen = Color(0, 255, 127)
+Color.Teal = Color(0, 128, 128)
+Color.MidnightBlue = Color(25, 25, 112)
+Color.DodgerBlue = Color(30, 144, 255)
+Color.LightSeaGreen = Color(32, 178, 170)
+Color.ForestGreen = Color(34, 139, 34)
+Color.SeaGreen = Color(46, 139, 87)
+Color.DarkSlateGray = Color(47, 79, 79)
+Color.DarkSlateGrey = Color(47, 79, 79)
+Color.LimeGreen = Color(50, 205, 50)
+Color.MediumSeaGreen = Color(60, 179, 113)
+Color.Turquoise = Color(64, 224, 208)
+Color.RoyalBlue = Color(65, 105, 225)
+Color.SteelBlue = Color(70, 130, 180)
+Color.DarkSlateBlue = Color(72, 61, 139)
+Color.MediumTurquoise = Color(72, 209, 204)
+Color.Indigo = Color(75, 0, 130)
+Color.DarkOliveGreen = Color(85, 107, 47)
+Color.CadetBlue = Color(95, 158, 160)
+Color.CornflowerBlue = Color(100, 149, 237)
+Color.MediumAquamarine = Color(102, 205, 170)
+Color.DimGray = Color(105, 105, 105)
+Color.DimGrey = Color(105, 105, 105)
+Color.SlateBlue = Color(106, 90, 205)
+Color.OliveDrab = Color(107, 142, 35)
+Color.SlateGray = Color(112, 128, 144)
+Color.SlateGrey = Color(112, 128, 144)
+Color.LightSlateGray = Color(119, 136, 153)
+Color.LightSlateGrey = Color(119, 136, 153)
+Color.MediumSlateBlue = Color(123, 104, 238)
+Color.LawnGreen = Color(124, 252, 0)
+Color.Aquamarine = Color(127, 255, 212)
+Color.Chartreuse = Color(127, 255, 0)
+Color.Gray = Color(128, 128, 128)
+Color.Grey = Color(128, 128, 128)
+Color.Maroon = Color(128, 0, 0)
+Color.Olive = Color(128, 128, 0)
+Color.Purple = Color(128, 0, 128)
+Color.LightSkyBlue = Color(135, 206, 250)
+Color.SkyBlue = Color(135, 206, 235)
+Color.BlueViolet = Color(138, 43, 226)
+Color.DarkMagenta = Color(139, 0, 139)
+Color.DarkRed = Color(139, 0, 0)
+Color.SaddleBrown = Color(139, 69, 19)
+Color.DarkSeaGreen = Color(143, 188, 143)
+Color.LightGreen = Color(144, 238, 144)
+Color.MediumPurple = Color(147, 112, 219)
+Color.DarkViolet = Color(148, 0, 211)
+Color.PaleGreen = Color(152, 251, 152)
+Color.DarkOrchid = Color(153, 50, 204)
+Color.YellowGreen = Color(154, 205, 50)
+Color.Sienna = Color(160, 82, 45)
+Color.Brown = Color(165, 42, 42)
+Color.DarkGray = Color(169, 169, 169)
+Color.DarkGrey = Color(169, 169, 169)
+Color.GreenYellow = Color(173, 255, 47)
+Color.LightBlue = Color(173, 216, 230)
+Color.PaleTurquoise = Color(175, 238, 238)
+Color.LightSteelBlue = Color(176, 196, 222)
+Color.PowderBlue = Color(176, 224, 230)
+Color.FireBrick = Color(178, 34, 34)
+Color.DarkGoldenrod = Color(184, 134, 11)
+Color.MediumOrchid = Color(186, 85, 211)
+Color.RosyBrown = Color(188, 143, 143)
+Color.DarkKhaki = Color(189, 183, 107)
+Color.Silver = Color(192, 192, 192)
+Color.MediumVioletRed = Color(199, 21, 133)
+Color.IndianRed = Color(205, 92, 92)
+Color.Peru = Color(205, 133, 63)
+Color.Chocolate = Color(210, 105, 30)
+Color.Tan = Color(210, 180, 140)
+Color.LightGray = Color(211, 211, 211)
+Color.LightGrey = Color(211, 211, 211)
+Color.Thistle = Color(216, 191, 216)
+Color.Goldenrod = Color(218, 165, 32)
+Color.Orchid = Color(218, 112, 214)
+Color.PaleVioletRed = Color(219, 112, 147)
+Color.Crimson = Color(220, 20, 60)
+Color.Gainsboro = Color(220, 220, 220)
+Color.Plum = Color(221, 160, 221)
+Color.BurlyWood = Color(222, 184, 135)
+Color.LightCyan = Color(224, 255, 255)
+Color.Lavender = Color(230, 230, 250)
+Color.DarkSalmon = Color(233, 150, 122)
+Color.PaleGoldenrod = Color(238, 232, 170)
+Color.Violet = Color(238, 130, 238)
+Color.Azure = Color(240, 255, 255)
+Color.Honeydew = Color(240, 255, 240)
+Color.Khaki = Color(240, 230, 140)
+Color.LightCoral = Color(240, 128, 128)
+Color.SandyBrown = Color(244, 164, 96)
+Color.Beige = Color(245, 245, 220)
+Color.MintCream = Color(245, 255, 250)
+Color.Wheat = Color(245, 222, 179)
+Color.WhiteSmoke = Color(245, 245, 245)
+Color.GhostWhite = Color(248, 248, 255)
+Color.LightGoldenrodYellow = Color(250, 250, 210)
+Color.Linen = Color(250, 240, 230)
+Color.Salmon = Color(250, 128, 114)
+Color.OldLace = Color(253, 245, 230)
+Color.Bisque = Color(255, 228, 196)
+Color.BlancheDalmond = Color(255, 235, 205)
+Color.Coral = Color(255, 127, 80)
+Color.CornSilk = Color(255, 248, 220)
+Color.DarkOrange = Color(255, 140, 0)
+Color.DeepPink = Color(255, 20, 147)
+Color.FloralWhite = Color(255, 250, 240)
+Color.Fuchsia = Color(255, 0, 255)
+Color.Gold = Color(255, 215, 0)
+Color.HotPink = Color(255, 105, 180)
+Color.Ivory = Color(255, 255, 240)
+Color.LavenderBlush = Color(255, 240, 245)
+Color.LemonChiffon = Color(255, 250, 205)
+Color.LightPink = Color(255, 182, 193)
+Color.LightSalmon = Color(255, 160, 122)
+Color.LightYellow = Color(255, 255, 224)
+Color.Magenta = Color(255, 0, 255)
+Color.MistyRose = Color(255, 228, 225)
+Color.Moccasin = Color(255, 228, 181)
+Color.NavajoWhite = Color(255, 222, 173)
+Color.Orange = Color(255, 165, 0)
+Color.OrangeRed = Color(255, 69, 0)
+Color.PapayaWhip = Color(255, 239, 213)
+Color.PeachPuff = Color(255, 218, 185)
+Color.Pink = Color(255, 192, 203)
+Color.Red = Color(255, 0, 0)
+Color.Seashell = Color(255, 245, 238)
+Color.Snow = Color(255, 250, 250)
+Color.Tomato = Color(255, 99, 71)
+Color.White = Color(255, 255, 255)
+Color.Yellow = Color(255, 255, 0)
+Color.RebeccaPurple = Color(102, 51, 153)
 
 
 @enum.unique
@@ -69,21 +274,22 @@ class Item:
 
     Arguments:
         - `enabled`: Enable or disable the UI element.
-        - `margin`: The margin of the UI element.
+        - `margin`: The margin of the UI element. Use a
+          :class:`Distance` instance to provide a margin.
         - `padding`: The padding of the UI element.
         - `position`: The position of the UI element on the view port.
           Defaults to top left of the screen.
         - `size`: The size of the UI element.
         - `color`: The colour of the UI element. Defaults to `black`.
     """
-    def __init__(self, enabled=True, position=None, size=None, color=None):
+    def __init__(self, enabled=True, margin=Distance.Zero, position=Point.TopLeft,
+                 size=None, color=Color.Black):
         self._settings = {}
         self.enabled = enabled
         self.margin = margin
-        self.padding = padding
-        self.position = Point(0.0, 0.0) if position is None else position
+        self.position = position
         self.size = size  # TODO: Does this need to be applied on the abstract element?
-        self.color = Color('#000000') if color is None else color
+        self.color = color
 
     @property
     def enabled(self):
